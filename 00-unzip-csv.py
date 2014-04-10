a2b1425eb8b392677a5710b8ccb6b6a03a3e07a8
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2014-04-10 16:51:40 -0400

    udpate

--- 00-unzip-csv.py
+++ 00-unzip-csv.py
@@ -1,5 +1,16 @@
 import os
+import glob
 import pandas as pd
+import zipfile
+import zlib
 
+td = '/Volumes/Echo/GIS/data/infogroup/CSV/'
 
-dirpath = '/Volumes/Echo/GIS/data/infogroup/CSV/'
+csvList = glob.glob(td + '*bus_2012.zip')
+
+for csvS in csvList:
+	#zipfile.ZipFile(csvS).extractall(td)
+	zipfile.ZipFile(csvS, allowZip64=True).extractall(td)
+
+#Weird ZIP Error - these ZIP files need to be uncompressed manually. 
+#NotImplementedError: compression type 9 (deflate64)
